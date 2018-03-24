4c4810094e6c846cf4c52cfe69795da0a5dad5c1
LapinsMorts <simon-2.lacasse@polymtl.ca>
2018-03-23 20:40:43 -0400

    junks

--- src/bot/MyBot.py
+++ src/bot/MyBot.py
@@ -5,7 +5,8 @@ class MyBot(Bot):
 
     def __init__(self):
         super().__init__()
-        self.next_ressource = (0, 0)
+        self.closest_ressource = (0, 0)
+        self.junks = []
 
     def get_name(self):
         # Find a name for your bot
@@ -15,13 +16,22 @@ class MyBot(Bot):
         # Your bot logic goes here
         super().turn(game_state, character_state, other_bots)
         gs_array = self.to_array(game_state)
-        self.next_ressource = self.find_next_ressource(gs_array)
+        self.closest_ressource = self.find_closest_ressource(character_state)
+        #print(str(self.closest_ressource))
         return self.commands.idle()
 
-    def find_next_ressource(self, gs_array):
-        print(gs_array)
-
-
+#(y,x)
+#{'location': (7, 1), 'carrying': 0, 'health': 100, 'name': 'My bot', 'points': 0, 'spawn': 0, 'status': 'alive', 'base': (7, 1), 'id': 1}
+    def find_closest_ressource(self, ch_state):
+        closest = (-1,-1)
+        for junk in self.junks:
+            if closest is (-1,-1):
+                closest = junk
+            else:
+                if abs(ch_state['location'][0] - closest[0]) + abs(ch_state['location'][1] -
+                        closest[1]) > abs(ch_state['location'][0] - junk[0]) + abs(ch_state['location'][1] - junk[1]):
+                    closest = junk
+        return closest
 
 
     def to_array(self, game_state):
@@ -31,6 +41,8 @@ class MyBot(Bot):
         for c in game_state:
             if c is not '\n':
                 array[i].append(c)
+                if c is 'J':
+                    self.junks.append((i, len(array[i])-1))
             else:
                 array.append([])
                 i += 1
